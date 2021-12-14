a3ae50d5eb39cbb5f26880f39b0db54235eef925
Margaret Farmer <margaret.farmer@gmail.com>
2021-12-14 18:12:09 -0500

    successful implementation of access token

--- src/Map.js
+++ src/Map.js
@@ -2,11 +2,13 @@
 import env from 'react-dotenv';
 import React from 'react';
 
-const MAP_KEY = process.env.API_KEY;
+const MAP_KEY = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+// console.log(MAP_KEY)
 
 export default function Map() {
 
   return (
-    <div><h1>{MAP_KEY}</h1></div>
+    <div><h1>Hi</h1></div>
   )
 }
+
