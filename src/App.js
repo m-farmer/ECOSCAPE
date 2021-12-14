a70902456cd94c9e0ab6a4b821a06401ea73a4c0
Margaret Farmer <margaret.farmer@gmail.com>
2021-12-14 13:25:12 -0500

    functioning chart output

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect } from 'react';
+import { select } from 'd3';
+import LineChart from "./LineChart";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <LineChart />
+
       </header>
+
+
     </div>
   );
 }
