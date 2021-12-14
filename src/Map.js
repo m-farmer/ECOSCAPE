3c80d39bb27b85cb0612a65fde5959e2ee4bb2a5
Margaret Farmer <margaret.farmer@gmail.com>
2021-12-14 18:52:16 -0500

    styled map to look like blueprint

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,31 @@
-// import {API_KEY} from '@env'
-import env from 'react-dotenv';
-import React from 'react';
+import React, {useState} from 'react';
+import mapboxgl from 'mapbox-gl'
+import ReactMapGL from 'react-map-gl';
 
 const MAP_KEY = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-// console.log(MAP_KEY)
 
 export default function Map() {
 
+  const [viewport, setViewport] = useState({
+    latitude: 40.7812,
+    longitude: -73.9665,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10
+  })
   return (
-    <div><h1>Hi</h1></div>
+    <div>
+
+      <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      mapStyle="mapbox://styles/m-m-farmer/ckx6q7bzz2hrq14nt0pl40ha2"
+      // for click and drag and zoom feature:
+      onViewportChange={viewport => {
+        setViewport(viewport);
+      }}
+      >
+      Markers Here
+      </ReactMapGL>
+    </div>
   )
 }
 
